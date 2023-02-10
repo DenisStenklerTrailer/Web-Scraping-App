9d6fd7298ab6c5dba70f701a7398dedfa2c6a423
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-10 12:23:43 +0100

    implemented email sending

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import selectorlib
+import smtplib
+import ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
@@ -9,7 +12,42 @@ def scrape(url):
     source = response.text
     return source
 
-if __name__ == "__main__":
-    print(scrape(URL))
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"] # tours is the name of the class in the extract.yaml file
+    return value
 
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "olga.trojer@gmail.com"
+    password = "mvcyzomecnqupgri"
+
+    reciever = "denis.stenkler@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, reciever, message)
+
+def store(extracted):
+    with open("data.txt", "w") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+
+    content = read(extracted)
+
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email(message="Hey, new event was found!")
 
